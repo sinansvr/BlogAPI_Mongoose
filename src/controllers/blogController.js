b9469e26e74f36234b791fdf9284c40373752276
sinans <svrsinan06@gmail.com>
2023-10-10 23:17:39 +0300

    CategoryController

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -2,62 +2,108 @@
 
 require("express-async-errors");
 
-const { BlogPost } = require("../models/blogModel");
+const { BlogCategory, BlogPost } = require("../models/blogModel");
+
+
+//-------------BlogCategory----------------
+
+module.exports.BlogCategory = {
+  list: async (req, res) => {
+    const data = await BlogCategory.find();
+
+    res.status(200).send({
+      error: false,
+      result: data,
+      count:data.length,
+    });
+  },
+
+  create: async (req, res) => {
+    const data = await BlogCategory.create(req.body);
+
+    res.status(201).send({
+      error: false,
+      result: data,
+      body: req.body,
+    });
+  },
+
+  read: async (req, res) => {
+    const data = await BlogCategory.findOne({ _id: req.params.id });
+
+    res.status(200).send({
+      error: false,
+      result: data,
+    });
+  },
+
+  update: async (req, res) => {
+    const data = await BlogCategory.updateOne({ _id: req.params.id }, req.body);
+
+    res.status(202).send({
+      error: false,
+      result: data,
+      body: req.body,
+      newData: await BlogCategory.findOne({ _id: req.params.id }),
+    });
+  },
+
+  delete: async (req, res) => {
+    const data = await BlogCategory.deleteOne({ _id: params.id });
+    
+    res.sendStatus(data.deletedCount >= 1 ? 204 : 404);
+  },
+};
+
+//-------------BlogPost----------------
 
 module.exports.BlogPost = {
-
   list: async (req, res) => {
-
     const data = await BlogPost.find();
 
     res.status(200).send({
       error: false,
-      count:data.length,
+      count: data.length,
       result: data,
       // body: req.body,
-
     });
-
   },
 
-  create: async(req,res)=>{
+  create: async (req, res) => {
 
-    const data= await BlogPost.create(req.body);
+    const data = await BlogPost.create(req.body);
 
     res.status(201).send({
-
-        error:false,
-        result:data,
-        body:req.body
-
-    })
-    
+      error: false,
+      result: data,
+      body: req.body,
+    });
   },
-  read: async(req,res)=> {
-    const data= await BlogPost.findOne({_id:req.params.postId})
+  read: async (req, res) => {
+
+    const data = await BlogPost.findOne({ _id: req.params.postId });
+
     res.status(200).send({
-      error:false,
-      result:data,
-    })
-
+      error: false,
+      result: data,
+    });
   },
-  update:async(req,res)=> {
+  update: async (req, res) => {
 
-    const data = await BlogPost.updateOne({_id:req.params.postId},req.body)
-    res.status(202).send({
-      error:false,
-      body:req.body,
-      result:data,
-      newData: await BlogPost.findOne({_id:req.params.postId})
-    })
-
-  },
-
-  delete: async(req,res)=>{
-
-    const data= await BlogPost.deleteOne({_id:req.params.postId})
-
-    res.sendStatus((data.deletedCount>=1)? 204 : 404)
+    const data = await BlogPost.updateOne({ _id: req.params.postId }, req.body);
     
+    res.status(202).send({
+      error: false,
+      body: req.body,
+      result: data,
+      newData: await BlogPost.findOne({ _id: req.params.postId }),
+    });
+  },
+
+  delete: async (req, res) => {
+
+    const data = await BlogPost.deleteOne({ _id: req.params.postId });
+
+    res.sendStatus(data.deletedCount >= 1 ? 204 : 404);
   },
 };
